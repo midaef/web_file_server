0e294baf4e661a82a7ff630a077ba50dfa4ed3b5
midaef <danil.present@mail.ru>
2020-02-02 22:01:25 +0800

    restructure(web_server): add some files and folders

---
+++ src/main/java/com/nameless/web_server/Main.java
@@ -0,0 +1,23 @@
+package com.nameless.web_server;
+
+/**
+ * This program can be use for your home PC or server.
+ * Web file server give you the ability to have permanent access to files on your computer.
+ * The author of this program does not call for its use as malicious software.
+ * Anyway the author does not bear any responsibility!
+ *
+ * @author Mikhailov Danil(midaef).
+ */
+
+/**
+ * This class is Main and it's do instance server class.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+		Settings settings = new Settings();
+		new Server(Integer.parseInt(settings.getPort()), settings.getPassword(), settings.getPath());
+    }
+
+}
---
+++ src/main/java/com/nameless/web_server/Server.java
@@ -0,0 +1,90 @@
+package com.nameless.web_server;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * This program can be use for your home PC or server.
+ * Web file server give you the ability to have permanent access to files on your computer.
+ * The author of this program does not call for its use as malicious software.
+ * Anyway the author does not bear any responsibility!
+ *
+ * @author Mikhailov Danil(midaef).
+ */
+
+/**
+ * This class use for start web file server.
+ * It's server can get request from browser and send to client.
+ *	1. If you want to start the server in your PC
+ *	2. You should to set-up the port in your router's settings.
+ *	3. Start program and set port and password in config.properties
+ *	4. Next, in the browser, connect in the format "localhost:port" or "ip:port".
+ *	5. Input password and any login.
+ * By the way, when you set-up port, you have to check port is not busy
+ */
+
+public class Server {
+
+	private Boolean shutdown = false;
+	private Integer port;
+	private String password;
+	private HashMap<String, String> users = new HashMap<>();
+
+	public Server(Integer port, String password, String ROOT_PATH) {
+		this.port = port;
+		this.password = password;
+		start(ROOT_PATH);
+	}
+
+	private void start(String ROOT_PATH) {
+		try {
+			InetAddress address = InetAddress.getByName("::");
+			try (ServerSocket serverSocket = new ServerSocket(port, 50, address)) {
+				System.out.println("[SERVER STARTED]-[" + getNowDate() + "]");
+				serverTask();
+				while (!shutdown) {
+					Socket socket = serverSocket.accept();
+					Session session = new Session(socket, users, password, ROOT_PATH);
+					session.start();
+				}
+			} catch (Exception e) {e.printStackTrace();}
+		} catch (Exception e) {e.printStackTrace();}
+		System.out.println("[Server stopped]-" + "[" + getNowDate() + "]");
+	}
+
+	private void serverTask() {
+		Runnable task = () -> {
+			while (!shutdown) {
+				System.out.print("\nCommands:\nexit - stop server\nusers - print users on server\n>>>");
+				String value = new Scanner(System.in).nextLine();
+				switch (value) {
+					case "exit":
+						System.out.println("[Server stopped]-" + "[" + getNowDate() + "]");
+						System.exit(1);
+					case "users":
+						for (String user : users.keySet()) {
+							System.out.println("User:" + users.get(user) + " | Token:" + user);
+						}
+						break;
+				}
+			}
+		};
+		Thread thread = new Thread(task);
+		thread.start();
+	}
+
+	private String getNowDate() {
+		Date now = new Date();
+		LocalDateTime nowDate = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String formatDateTime = nowDate.format(formatter);
+		return formatDateTime;
+	}
+
+}
